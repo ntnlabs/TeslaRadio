9ee55436bdcd0f176f1fc6a4fa4d20b156f62754
ntnlabs <ntnlabs@gmail.com>
2020-12-29 19:26:13 +0000

    test2 update

--- test2.py
+++ test2.py
@@ -3,40 +3,33 @@ from pyky040 import pyky040
 import threading
 import time
 
+# define variables
+Volume = 0
+Menu_pos = 0
+
 # Define your callback
 def my_callback_rotate(scale_position):
-    print('Hello world! The scale position is {}'.format(scale_position))
+#    print('Hello world! The scale position is {}'.format(scale_position))
+    global Volume
+    Volume = scale_position
 
 def my_callback_press(state):
-    print('Hello world! The button has been pressed {}'.format(state))
+#    print('Hello world! The button has been pressed {}'.format(state))
+    global Menu_pos
+    Menu_pos = 1
 
-# Init the encoder pins
 my_encoder = pyky040.Encoder(CLK=5, DT=6, SW=13)
-
-# Or the encoder as a device (must be installed on the system beforehand!)
-# my_encoder = pyky040.Encoder(device='/dev/input/event0')
-
-# Setup the options and callbacks (see documentation)
 my_encoder.setup(scale_min=0, scale_max=100, step=1, chg_callback=my_callback_rotate, sw_callback=my_callback_press, sw_debounce_time=300)
-
-# Create the thread
 my_thread = threading.Thread(target=my_encoder.watch)
-
-# Launch the thread
 my_thread.start()
 
 # Do other stuff
-print('Other stuff...')
+print('Display Init')
 while True:
-    print('Looped stuff...')
-    time.sleep(1000)
-# ... this is also where you can setup other encoders!
+    print('--------------------')
+    print('|                  |')
+    print('|      {:<3}       |'.format(Volume))
+    print('|                  |')
+    print('--------------------')
 
-# Mess with the encoder...
-# > Other stuff...
-# > Looped stuff...
-# > Hello world! The scale position is 1
-# > Hello world! The scale position is 2
-# > Hello world! The scale position is 3
-# > Looped stuff...
-# > Hello world! The scale position is 2
+    time.sleep(100)
